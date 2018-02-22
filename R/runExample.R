ee7f0ee7ce5e5f26415e16d1aa1b47624aa50f4c
MABadge <mabadgeley@gmail.com>
2018-02-21 18:32:25 -0800

    patch "inst" to "init" so everything in init will be copied into the top-level package directory upon package installation.

--- R/runExample.R
+++ R/runExample.R
@@ -1,10 +1,42 @@
+#' Run candi cad example application.
+#'
+#' Launch a Shiny app to demo the CAD clinical trial app.
+#'
+#' @examples
+#' if (interactive()) {
+#'    runCAD()
+#' }
 #' @export
 runCAD <- function() {
-    appDir <- system.file("examples", "CANDI_CAD", package="candi")
+    appDir <- system.file("init", "examples", "CANDI_CAD", package="candi")
     if (appDir == "") {
         stop("Could not find example directory. Try re-installing `candi`.", call.=FALSE)
     }
 
 
+    shiny::runApp(appDir, display.mode = "normal")
+}
+
+#' @export
+runExample <- function() {
+    validExamples <- list.files(system.file("examples", package = "candi"))
+
+    validExamplesMsg <-
+        paste0(
+            "Valid examples are: '",
+            paste(validExamples, collapse = "', '"),
+            "'")
+
+    # if an invalid example is given, throw an error
+    if (missing(example) || !nzchar(example) ||
+        !example %in% validExamples) {
+        stop(
+            'Please run `runExample()` with a valid example app as an argument.\n',
+            validExamplesMsg,
+            call. = FALSE)
+    }
+
+    # find and launch the app
+    appDir <- system.file("init", "examples", example, package="candi")
     shiny::runApp(appDir, display.mode = "normal")
 }
