dc66b7ee1001dc7e7784f1e1ea0e3507fe47cff4
MABadge <mabadgeley@gmail.com>
2018-03-13 02:16:50 -0700

    Change FS input interfacing
    refactor fsio flags to individual scripts instead of pkg
    
    na

--- R/candi-package.R
+++ R/candi-package.R
@@ -5,14 +5,3 @@
 #' @name candi
 #' @docType package
 "_PACKAGE"
-
-
-
-.PROJECT_DIR <- function() {
-    .PROJECT_DIR <- switch (Sys.info() %>% `[`("sysname"),
-                            "Windows" = "P:/candi",
-                            "Linux"   = "/media/marcus/Projects/candi")
-    # Windows Bash subsystem edge case
-    if (Sys.info()["sysname"] == "Linux" && stringr::str_detect(Sys.info()["release"], "Microsoft")) {.PROJECT_DIR <- "/mnt/p/candi"}
-    return(.PROJECT_DIR)
-}
