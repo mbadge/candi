062c2cf44eea2aecb756d5d0b98c892d3d31f32a
MABadge <mabadgeley@gmail.com>
2018-04-27 22:03:30 -0700

    adapt io interface for RAD image app. see #12 !2.

--- inst/examples/RAD_image/global.R
+++ inst/examples/RAD_image/global.R
@@ -5,30 +5,32 @@ library(magrittr)
 options(shiny.reactlog=TRUE)  # allows reactivity inspection in browser with Ctrl-F3
 
 
-# Flags -----------------------
+# FSIO ----
 # pkg data
-data(test_df, package="cxrTargetDiff")
-data(dx_df, package="cxrTargetDiff")
+data(test_df, package="cxrTargetDiff")  # EHR data for test app test cases
 
-# fsio
-kDIR_USR_INPT <- "/www/app_data/app_data_cxrTargetDiff/usr_inpt/target_difficulty_trial"  # Directory with user input records
-kDIR_LARGE_IMGS <- "/www/app_data/app_data_cxrTargetDiff/large_jpgs/"
-
-# Dir for non-user input user session data; event logger with timestamps
-kDIR_USR_LOG <- file.path(kDIR_USR_INPT, "log")
+kDIR_LARGE_IMGS <- candiOpt(large_img_dir)
+kDIR_USR_INPT <- candiOpt(usr_input_dir)
+kDIR_USR_LOG <- file.path(kDIR_USR_INPT, "log")  # Dir for non-user input user session data - timestamped event logger
 # Usr_Inpt is referenced by the application logic, whereas log files should only be used in downstream analysis
+
+# FLAGS ----
+kDXS_CHR <- c('Cardiomegaly', 'Emphysema', 'PleuralEffusion',
+              'HerniaHiatal', 'Nodule', 'PulmonaryAtelectasis',
+              'Pneumonia', 'PulmonaryEdema', 'Consolidation',
+              'CathetersIndwelling', 'TechnicalQualityOfImageUnsatisfactory',
+              'LungHypoinflation', 'LungHyperdistention')
 
 # Preconditions ----
 stopifnot(dir.exists(kDIR_USR_INPT),
           dir.exists(kDIR_LARGE_IMGS),
           dir.exists(kDIR_USR_LOG))
+stopifnot(all(kDXS_CHR %in% names(test_df)))
 
 
 # Main ----
 # use diagnoses from pkg data dx_df for user impression ui
-kDXS_CHR <- dx_df$targets
 kDXS_CHR %<>% purrr::set_names(., MyUtils::str_case_title(.))
-rm(dx_df)
 
 # Check data.table and image file overlap
 large_img_ids <- list.files(kDIR_LARGE_IMGS, pattern = "*.jpg", full.names=TRUE) %>% MyUtils::fp_stem()
