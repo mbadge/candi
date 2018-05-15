9419dcf7fc91efe72749f95eecfe865728dc9737
MABadge <mabadgeley@gmail.com>
2018-05-15 15:44:31 -0700

    Additional module naming schema and pilot schema compliant module code generator
    Extend module naming schema:
    module identifiers should follow the input id schema since they're used most like input ids
    ui.R: <ui_module_fxn>("namespace_id")
    server.R: callModule(<server_module_fxn>, "namespace_id")
    
    make code generator to return module components.  bquoting both definitions fails, so I construct them
    separately and then enlist the return

--- inst/module_demos/naming_schema.R
+++ inst/module_demos/naming_schema.R
@@ -18,12 +18,15 @@ shinyApp(
 # Modules ----
 # ui fxn: lowerCamelCase ending w/ Input/Output/UI
 # server fxn: lowerCamelCase ending w/ Module
-usrMsgOutput <- function(id, msg) {
+# namespace id variable: snake case
+#  * ui.R: <ui_module_fxn>("namespace_id")
+#  * server.R: callModule(<server_module_fxn, "namespace_id")
+usrMsgOutput <- function(id) {
     ns <- NS(id)
     textOutput(ns("msgText"))
 }
-usrMsgModule <- function(input, output, session, userMsg) {
-    output$msgText <- renderText(expr = userMsg())
+usrMsgModule <- function(input, output, session, usrMsg) {
+    output$msgText <- renderText(expr = usrMsg())
 }
 shinyApp(
     ui = fluidPage(
@@ -31,6 +34,39 @@ shinyApp(
         usrMsgOutput("user_msg")
     ),
     server = function(input, output, session) {
-        callModule(usrMsgModule, "user_msg", userMsg = reactive(input$user_msg))
+        callModule(usrMsgModule, "user_msg", usrMsg = reactive(input$user_msg))
     }
 )
+
+
+
+
+generateCode_module <- function(module_id) {
+    # module id should equate to the key arg passed into the module invocations
+    stopifnot(is.character(module_id))
+
+    ui_fxn_nm <- str_c(str_case_camel(module_id), "Output")
+    server_fxn_nm <- str_c(str_case_camel(module_id), "Module")
+
+    ui <- bquote(
+        .(ui_fxn_nm) <- function(id) {
+            ns <- NS(id)
+            textOutput(ns( .(module_id) ))
+        }
+    )
+
+    server <- bquote(
+        .(server_fxn_nm) <- function(input, output, session) {
+            output[[ .(module_id) ]] <- renderText(expr = .(str_case_camel(module_id)) () )
+        }
+    )
+
+    list(ui = ui, server = server)
+}
+generateCode_module("user_msg")
+
+
+"userMsgOutput" <- function(id) {
+    ns <- NS(id)
+    textOutput(ns(msgText))
+}
