c8a56ab0cb6da142dd5198fb5a7cbc249ea983d5
MABadge <mabadgeley@gmail.com>
2018-05-16 09:31:17 -0700

    add image id manipulations from ProjUtilsRads

--- R/util_ids.R
+++ R/util_ids.R
@@ -49,3 +49,126 @@ imgIds2Cases <- function(img_ids) {
         str_split(pattern = "_") %>%
         map_chr(2) %>% unique() %>% as.integer()
 }
+
+
+
+#! I use impure functions in this script so that I can configure the image ids right here!
+kIMG_ID_X_VARS <- c("dataset", "case", "image")
+kIMG_ID_S_CMPD <- c("img_id")
+
+#' Image Identifier Helper Transformations
+#'
+#' Image Ids can be represented in 2 ways:
+#' \enumerate{
+#'     \item chr(1) unique compound key
+#'     \item chr(n) vector of individual id component variables
+#' }
+#'
+#' The default names of id vars are configured with the global `kID_CHRS`,
+#' which is defined in this img id helpers script
+#'
+#' Mutating Fxn to separate `img_id` column of a data.frame into separate component identifiers.
+#'
+#' Assumes the project schema of img_id=<dataset>_<case>_<image>
+#' Attempts to attach original attributes onto the product (except names and spec)
+#'
+#' dataset, case, image cols.
+#'
+#' Unite id components into a single unique image identifier.
+#'
+#' Convenience wrapper to directly operate on a joint id as if it were split
+#'
+#' @param df data.frame with column 'img_id' or columns `img_vars`
+#' @param img_id_cmpd s_chr compound key identifier representation
+#' @param img_id_vars x_chr names of individual tidy id variables
+#' @param ... args passed to \code{\link[tidyr]{separate}} or \code{\link[tidyr]{unite}}
+#' @param f function to call on df after separating ids
+#'
+#' @return data.frame with transformed id column representation
+#'
+#' @examples
+#' data(lut_img_id2original)
+#' sep_id(lut_img_id2original)
+#' sep_id(lut_img_id2original, remove=FALSE)
+#' sep_id(lut_img_id2original) %>% unite_id
+#' sep_id(bind_rows(lut_img_id2original, lut_img_id2original))  # Expect warning
+#' @name img_id
+NULL
+
+
+#' @export
+#' @rdname img_id
+sep_id <- function(df,
+                   img_id_cmpd = kIMG_ID_S_CMPD,
+                   img_id_vars = kIMG_ID_X_VARS,
+                   sep = "_", remove = TRUE, convert = TRUE, ...
+)
+{
+    # Preconditions
+    stopifnot(is.data.frame(df))
+    stopifnot(img_id_cmpd %in% names(df))
+
+    # i.f.f img_ids are unique, save rowname attr as the scalar compound key
+    if (!any(duplicated(df[[img_id_cmpd]]))) {
+        df <- df %>% tibble::remove_rownames()
+        suppressWarnings(rownames(df) <- df[[img_id_cmpd]])
+    } else {
+        warning("Duplicate img_ids exist in df")
+    }
+
+    # save the original df attrs to add back to new return data.frame
+    attrs <- df %>% attributes
+    my_attr_nms <- setdiff(names(attrs), c("names", "spec"))  # don't include default attrs
+    attrs <- attrs[my_attr_nms]
+
+    # ENGINE
+    out <- df %>%
+        tidyr::separate(!!img_id_cmpd, img_id_vars,
+                        sep=sep, remove=remove, convert=convert, ...)
+
+    attributes(out) <- modifyList(attributes(out), attrs)
+    out
+}
+
+#' @export
+#' @rdname img_id
+unite_id <- function(df,
+                     img_id_cmpd = kIMG_ID_S_CMPD,
+                     img_id_vars = kIMG_ID_X_VARS,
+                     sep="_", remove=TRUE) {
+    # Preconditions
+    stopifnot(is.data.frame(df))
+    stopifnot(all(img_id_vars %in% names(df)))
+
+    # Save attrs
+    attrs <- df %>% attributes
+    my_attr_nms <- setdiff(names(attrs), c("names", "spec"))  # don't include default attrs
+    attrs <- attrs[my_attr_nms]
+
+    # ENGINE
+    out <- df %>%
+        tidyr::unite(!!img_id_cmpd, dplyr::one_of(img_id_vars),
+                     sep=sep, remove=remove)
+
+    attributes(out) <- modifyList(attributes(out), attrs)
+    out
+}
+
+#' @export
+#' @rdname img_id
+with_sep <- function(df, f, ...) {
+    df %>%
+        sep_id() %>%
+        f(., ...) %>%
+        unite_id()
+}
+
+#' @export
+#' @rdname img_id
+with_united <- function(df, f, ...) {
+    df %>%
+        unite_id() %>%
+        f(., ...) %>%
+        sep_id()
+}
+
