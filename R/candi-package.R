dda1438032537122d24f98a860dd05dec627ce6c
MABadge <mabadgeley@gmail.com>
2018-05-19 18:24:32 -0700

    document pkg data

--- R/candi-package.R
+++ R/candi-package.R
@@ -1,7 +1,25 @@
-#' candii
+#' candi
 #'
 #' Demonstrate shiny candi radiograph dashboard implementations.
 #'
 #' @name candi
 #' @docType package
 "_PACKAGE"
+
+#' case-major data.frame with patient and diagnostic variables
+#'
+#' @name cases
+#' @docType data
+"cases"
+
+#' radiograph-major data.frame with technical specs and CNN PCs and pYs
+#'
+#' @name radiographs
+#' @docType data
+"radiographs"
+
+#' chr(113) with image identifiers to be tested
+#'
+#' @name test_imgs
+#' @docType data
+"test_imgs"
